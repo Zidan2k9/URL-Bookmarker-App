e5b5389592de4966ab086bd69f60bc80a677b2c5
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-01-11 22:33:47 +0000

    Bookmarks added by the user via the form are stored in local storage

--- js/main.js
+++ js/main.js
@@ -20,14 +20,41 @@ function saveBookmark(e){
        url:siteUrl
    }
 
-   console.log(bookmark);
+   //test if bookmarks is null
+   if(localStorage.getItem('bookmarks') === null){
 
-   //local storage test
-   localStorage.setItem('test','hello world');
+    //init array
+    var bookmarks = [];
 
-   console.log(localStorage.getItem('test'));
+    //add to array
+    bookmarks.push(bookmark);
+
+    //add to local storage
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+   }
+   else{
+       //fetch bookmarks from local storage
+       var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+       //add bookmark to array
+       bookmarks.push(bookmark);
+
+       //reset to local storage
+
+       localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+   }
 
     //prevent the form from submitting
 
     e.preventDefault();
 }
+
+/*console.log(bookmark);
+
+   //local storage test
+   localStorage.setItem('test','hello world');
+
+   console.log(localStorage.getItem('test'));
+   localStorage.removeItem('test');
+   console.log(localStorage.getItem('test')); */
