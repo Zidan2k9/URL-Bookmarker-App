4270da41df168e784eb3ef3a5ed7bbc1eb8f0362
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-01-11 17:32:59 +0000

    Data can be stored in temp local storage successfully

--- js/main.js
+++ js/main.js
@@ -2,6 +2,32 @@
 
 document.getElementById('siteForm').addEventListener('submit',saveBookmark);
 
-function saveBookmark(){
+
+//saving the bookmark
+function saveBookmark(e){
     console.log('clicked the button');
+
+    //get form vars
+
+    var siteName = document.getElementById('siteName').value;
+    //console.log(siteName);
+
+    var siteUrl = document.getElementById('siteURL').value;
+    //console.log(siteUrl);
+
+   var bookmark = {
+       name:siteName,
+       url:siteUrl
+   }
+
+   console.log(bookmark);
+
+   //local storage test
+   localStorage.setItem('test','hello world');
+
+   console.log(localStorage.getItem('test'));
+
+    //prevent the form from submitting
+
+    e.preventDefault();
 }
