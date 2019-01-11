536c387864e4dcfc8bf893246791e91d12613c00
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-01-11 16:56:09 +0000

    Changed bootstrap and jquery versions due to the following error in the chrome browser : Uncaught Error: Bootstrap's JavaScript requires jQuery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Listen for submit button click
+
+document.getElementById('siteForm').addEventListener('submit',saveBookmark);
+
+function saveBookmark(){
+    console.log('clicked the button');
+}
